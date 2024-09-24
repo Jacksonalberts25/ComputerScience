4766b58ef9b34c807f0a0eaea3bcfa4be44a034e
Jacksonalberts25 <834071@mystma.org>
2024-09-24 15:19:18 -0500

    sept24

---
+++ Assignments/AnimalSurvey.py
@@ -0,0 +1,16 @@
+favorite_animal = input("whats your favorite animal?\n>")
+print("your favorite aniaml is " + favorite_animal)
+color = input("what color is this animal?\n>")
+print("the walrus is " + color)
+live = input("where does the animal live?\n>")
+print("the animal lives at " + live)
+food = input("what is their favorite food?\n>")
+print("their favorite food is " + food)
+
+print(favorite_animal + " " + color + " " + live + " " + food)
+
+
+
+
+
+
