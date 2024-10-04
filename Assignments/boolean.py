ea2d175a38f5cb57105bc884e1f20cdd6ecfc486
Jacksonalberts25 <834071@mystma.org>
2024-10-04 14:53:33 -0500

    boolean

---
+++ Assignments/boolean.py
@@ -0,0 +1,22 @@
+print(4 >= 2)
+print(1993 == 1993)
+print(101093 >= 2094358)
+print(1000000 == 1000001)
+print(14 ==14)
+
+
+
+answer = int(input("give a whole number that is less than 5.\n"))
+print(answer < 5)
+
+answer = int(input("give a number bigger than 10.\n"))
+print(answer > 10)
+
+answer = int(input("please enter a number one number bigger than 9.\n"))
+print(answer == 10)
+      
+
+
+
+
+            
